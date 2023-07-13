4cd3d7ad20185d9685260f4d936cbb774414ca1d
daom bae <da6970@gmail.com>
2023-07-13 09:16:54 +0900

    배경프리미어영상

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { createContext, useEffect, useState } from 'react';
-import './index.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Footer from './components/footer';
-import Header from './components/header';
-import StatusBar from './components/statusbar';
-import EventPage from './pages/Event';
-import Homepage from './pages/Home';
-// import ReactPlayer from "react-player";
-import Mypage from './pages/Mypage';
-import Nfttest from './pages/Nfttest';
-import AdminPage from './pages/admin';
-import { n_abi, n_addr, t_abi, t_addr } from './raffletest.config';
-import Web3 from 'web3';
-import LoginPage from './pages/login';
-import TicketBooking from './pages/Ticket2';
+import React from "react";
+import { createContext, useEffect, useState } from "react";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import StatusBar from "./components/statusbar";
+import EventPage from "./pages/Event";
+import Homepage from "./pages/Home";
+import ReactPlayer from "react-player";
+import Mypage from "./pages/Mypage";
+import Nfttest from "./pages/Nfttest";
+import AdminPage from "./pages/admin";
+import { n_abi, n_addr, t_abi, t_addr } from "./raffletest.config";
+import Web3 from "web3";
+import LoginPage from "./pages/login";
+import TicketBooking from "./pages/Ticket2";
 
 export const AppContext = createContext();
 
-const admin = '0x1f6D70acBd7B09096717fd5625783F78AF685A5a';
+const admin = "0x1f6D70acBd7B09096717fd5625783F78AF685A5a";
 function App() {
   const [temp, setTemp] = useState();
   const [account, setAccount] = useState();
@@ -62,24 +62,50 @@ function App() {
         web3,
         token_c,
         nft_c,
-      }}
-    >
-      <BrowserRouter>
-        <div className="iphone-container min-h-[844px]">
-          <StatusBar />
-          <Header />
-          <Routes>
-            <Route exact path="/" element={<Homepage />} />
-            <Route path="/Ticket" element={<TicketBooking />} />
-            <Route path="/Event" element={<EventPage />} />
-            <Route path="/Mypage" element={<Mypage />} />
-            <Route path="/LoginPage" element={<LoginPage />} />
-            <Route path="/AdminPage" element={<AdminPage admin={admin} />} />
-            <Route path="/nft" element={<Nfttest />} />
-          </Routes>
-          <Footer />
-        </div>
-      </BrowserRouter>
+      }}>
+      <div>
+        <ReactPlayer
+          url="/Videos/video.mp4"
+          playing={true}
+          loop={true}
+          muted={true}
+          width="100%"
+          height="auto"
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        />
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            backgroundColor: "rgba(0,0,0,0.15)",
+          }}
+        />
+
+        <BrowserRouter>
+          <div className="iphone-container min-h-[844px]">
+            <StatusBar />
+            <Header />
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route path="/Ticket" element={<TicketBooking />} />
+              <Route path="/Event" element={<EventPage />} />
+              <Route path="/Mypage" element={<Mypage />} />
+              <Route path="/LoginPage" element={<LoginPage />} />
+              <Route path="/AdminPage" element={<AdminPage admin={admin} />} />
+              <Route path="/nft" element={<Nfttest />} />
+            </Routes>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </div>
     </AppContext.Provider>
   );
 }
